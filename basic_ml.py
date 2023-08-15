113f060987ba838a104b332e53aa8a2a551f96c8
Pratik014 <pratikmore014@gmail.com>
2023-08-15 10:51:28 +0530

    new code

--- basic_ml.py
+++ basic_ml.py
@@ -8,7 +8,7 @@ import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import ElasticNet
 
-from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,accuracy_score
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,accuracy_score,roc_auc_score
 from sklearn.model_selection import train_test_split
 
 import argparse
@@ -19,12 +19,14 @@ def get_data():
     df=pd.read_csv(url,sep=";")
     return df
 
-def evaluate(y_true,y_pred):
+def evaluate(y_true,y_pred,y_pred_prob):
     #mse=mean_squared_error(y_true,y_pred)
     #mae=mean_absolute_error(y_true,y_pred)
     #rmse=np.sqrt(mean_squared_error(y_true,y_pred))
     accuracy=accuracy_score(y_pred,y_true)
-    return accuracy
+    roc_score=roc_auc_score(y_pred_prob,y_true,multi_class='ovr' )
+    
+    return accuracy,roc_score
 
 
 
@@ -43,13 +45,23 @@ def main(n_estimators,max_depth,min_samples_split,max_samples):
 
     #mse,mae,rmse=evaluate(y_pred,y_test)
     #print(f'mse {mse},mae {mae},rmse {rmse}')
+    with mlflow.run_start():
+        rnd=RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth,min_samples_split=min_samples_split,max_samples=max_samples)
+        rnd.fit(X_train,y_train)
+        y_pred=rnd.predict(X_test)
+        y_pred_prob=rnd.predict_log_proba(X_test)
+    
+        mlflow.sklearn.log_model(rnd,'model')
 
-    rnd=RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth,min_samples_split=min_samples_split,max_samples=max_samples)
-    rnd.fit(X_train,y_train)
-    y_pred=rnd.predict(X_test)
-
-    accuracy=evaluate(y_pred,y_test)
-    print(f'accuracy {accuracy}')
+        roc_auc_score,accuracy=evaluate(y_pred,y_test,y_pred_prob)
+        
+        mlflow.log_metric(accuracy)
+        
+        mlflow.log_param("n_estimators",n_estimators)
+        mlflow.log_param("max_depth",max_depth)
+        mlflow.log_param("min_samples_split",min_samples_split)
+        mlflow.log_param("max_samples",max_samples)        
+        print(f'accuracy {accuracy}')
 
 if __name__ == '__main__':
     arg=argparse.ArgumentParser()
